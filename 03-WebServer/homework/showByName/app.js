ee5b948d78ef248aa4103d7647c84feefacb4077
carlamadeo <carlamadeo@gmail.com>
2022-09-15 22:38:12 -0300

    03-WebServer completed

--- 03-WebServer/homework/showByName/app.js
+++ 03-WebServer/homework/showByName/app.js
@@ -1,4 +1,20 @@
-var fs  = require("fs")
-var http  = require("http")
+const fs = require('fs');
+const http = require('http');
+const path = require('path');
 
-// Escribí acá tu servidor
+//Escribí acá tu servidor
+http.createServer( (req, res) => {
+
+  const imgURL = path.join(__dirname, 'images', `${req.url}.jpg`);
+  fs.readFile(imgURL, (err, data) => {
+    if(err){
+      res.writeHead(404, {'Content-Type': 'text/plain'}, 'utf8');
+      res.end('Recurso no encontrado');
+    }
+    else{
+      res.writeHead(200, {'Content-Type': 'image/jpg'});
+      res.end(data);
+    }
+  });
+  
+}).listen(3000, '127.0.0.1');
