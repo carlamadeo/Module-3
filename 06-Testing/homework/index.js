abe0eab2d92f1f2afa9af4c6eb432d919d787cfb
carlamadeo <carlamadeo@gmail.com>
2022-09-28 21:03:17 -0300

    06-Testing completed

--- 06-Testing/homework/index.js
+++ 06-Testing/homework/index.js
@@ -9,9 +9,61 @@ app.get('/', (req, res) => {
   });
 });
 
+app.get('/test', (req, res) => {
+  res.send({
+    message: 'test',
+  });
+});
+
+app.post('/sum', (req, res) => {
+  res.send({
+    result: req.body.a + req.body.b,
+  });
+});
+
 app.post('/product', (req, res) => {
   res.send({
-    result: req.body.a / req.body.b,
+    result: req.body.a * req.body.b,
+  });
+});
+
+const sumArray = (array, num) => {
+  if(!Array.isArray(array) || typeof num != 'number') throw new TypeError('Types do not match');
+  for(let i = 0; i < array.length; i++) {
+    for(let j = i + 1; j < array.length; j++) {
+      if(array[i] !== array[j]) {
+        if(array[i] + array[j] === num) return true;
+      }
+    }
+  }
+  return false;
+}
+
+const pluck = (array, prop) => {
+
+  return array.map(p => p[prop]);
+}
+
+app.post('/sumArray', (req, res) => {
+  const {array, num} = req.body;
+  res.send({
+    result: sumArray(array, num)
+  })
+});
+
+app.post('/numString', (req, res) => {
+  const {string} = req.body;
+  if(typeof string !== 'string' || string === '') res.status(400);
+  return res.send({
+    result: string.length
+  });
+});
+
+app.post('/pluck', (req, res) => {
+  const {array, prop} = req.body;
+  if(!Array.isArray(array) || prop === '') res.status(400);
+  return res.send({
+    result: pluck(array, prop)
   });
 });
 
--- 06-Testing/homework/tests/index.spec.js
+++ 06-Testing/homework/tests/index.spec.js
@@ -1,22 +1,22 @@
 const session = require('supertest-session');
+//const {expect} = require('chai');
 const app = require('../index.js'); // Importo el archivo de entrada del server de express.
-
 const agent = session(app);
 
 describe('Test de APIS', () => {
   describe('GET /', () => {
     it('responds with 200', () => agent.get('/').expect(200));
-    it('responds with and object with message `hola`', () =>
-        agent.get('/').then((res) => {
-          expect(res.body.message).toEqual('hola');
-        }));
+    it('responds with an object with message `hola`', () =>
+      agent.get('/').then((res) => {
+        expect(res.body.message).toEqual('hola');
+      }));
   });
 
   describe('GET /test', () => {
     it('responds with 200', () => agent.get('/test').expect(200));
-    it('responds with and object with message `test`', () =>
+    it('responds with an object with message `test`', () =>
       agent.get('/test').then((res) => {
-        expect(res.body.message).toEqual('hola');
+        expect(res.body.message).toEqual('test');
       }));
   });
 
@@ -31,7 +31,7 @@ describe('Test de APIS', () => {
     );
   });
 
-  describe('POST /producto', () => {
+  describe('POST /product', () => {
     it('responds with 200', () => agent.post('/product').expect(200));
     it('responds with the product of 2 and 3', () =>
       agent.post('/product')
@@ -43,13 +43,46 @@ describe('Test de APIS', () => {
   });
 
   describe('POST /sumArray', () => {
-    it('responds with 200', () => agent.get('/test').expect(200));
-    it('responds with and object with message `test`', () =>
+    it('responds with 200', () => agent.post('/sumArray').send({array: [], num: 1}).expect(200));
+    it('responds with true', () =>
       agent.post('/sumArray')
-        .send({array: [2,5,7,10,11,15,20], num: 13})
+        .send({array: [2, 5, 7, 10, 11, 15, 20], num: 13})
         .then((res) => {
           expect(res.body.result).toEqual(true);
-      }));
+        }));
+    it('responds with false', () =>
+      agent.post('/sumArray')
+        .send({array: [2, 5, 7, 10, 11, 15, 20], num: 100})
+        .then((res) => {
+          expect(res.body.result).toEqual(false);
+        }));
+    it('should not add the same number twice', () =>
+      agent.post('/sumArray')
+        .send({array: [2, 8, 8, 10, 11, 15, 20], num: 16})
+        .then((res) => {
+          expect(res.body.result).toEqual(false);
+        }));
+  });
+
+  describe('POST /numString', () => {
+    it('responds with 200', () => agent.post('/numString').send({string: 'string'}).expect(200));
+    it('responds with 4', () =>
+      agent.post('/numString').send({string: 'hola'}).then((res) => expect(res.body.result).toEqual(4)));
+    it('responds with a status 400 (bad request) if the parameter is a number', () =>
+      agent.post('/numString').send({string: 9}).expect(400));
+    it('responds with a status 400 (bad request) if the parameter is an empty string', () =>
+      agent.post('/numString').send({string: ''}).expect(400));
+  });
+
+  describe('POST /pluck', () => {
+    const array = [{title: 'Libro uno', stock: 10}, {title: 'Libro dos', stock: 2}, {title: 'Libro tres', stock: 0}];
+    it('responds with 200', () => agent.post('/pluck').send({array: array, prop: 'title'}).expect(200));
+    it('responds with titles', () =>
+      agent.post('/pluck').send({array: array, prop: 'title'}).then((res) => expect(res.body.result).toEqual(['Libro uno', 'Libro dos', 'Libro tres'])));
+    it('responds with a status 400 (bad request) if the parameter is not an array', () =>
+      agent.post('/pluck').send({array: 64, prop: 'title'}).expect(400));
+    it('responds with a status 400 (bad request) if the parameter is an empty string', () =>
+      agent.post('/pluck').send({array: array, prop: ''}).expect(400));
   });
 
 });
